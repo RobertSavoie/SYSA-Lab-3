b4851524915a9185058781e0deafe10b8f4fb991
megan-gwynn <105083666+megan-gwynn@users.noreply.github.com>
2022-11-06 14:59:05 -0500

    Update ellipse_test.py

---
+++ Lab3/test/ellipse_test.py
@@ -0,0 +1,37 @@
+# Megan Gagliardi & Rob Savoie
+# SYSA 3204
+# 11/5/2022
+# Lab 3 - Group 15
+
+import unittest
+
+from Lab3.app.ellipse import Ellipse
+
+
+class TestEllipse(unittest.TestCase):
+    def setUp(self):
+        self.ellipse = Ellipse()
+
+    def tearDown(self):
+        print("\n end of test ", self.shortDescription())
+
+    def test_ellipse_area_radius_less_than_zero(self):
+        """Exception raised when radius of ellipse < 0"""
+        e = self.ellipse.ellipse_area(-2.3, 0)
+        self.assertEqual(e.radius, -2.3, 0)
+        self.assertRaises(ValueError)
+
+    def test_ellipse_area_greater_than_zero(self):
+        """Test area of ellipse when radius >= 0"""
+        e = Ellipse(5, 7)
+        self.assertEqual(self.ellipse.ellipse_area(5, 7), 109.96)
+
+    def test_ellipse_area_invalid_input(self):
+        """Exception raised when area of ellipse is invalid, boolean, or text"""
+        e = self.ellipse.ellipse_area('hello')
+        self.assertEqual(e.radius, 'hello')
+        self.assertRaises(TypeError)
+
+
+if __name__ == '__main__':
+    unittest.main()
