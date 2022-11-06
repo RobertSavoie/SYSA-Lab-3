b544fc55d11698934f1a56b71c6a094680d6c3d0
megan-gwynn <105083666+megan-gwynn@users.noreply.github.com>
2022-11-06 14:59:37 -0500

    Update ellipse.py

---
+++ Lab3/app/ellipse.py
@@ -0,0 +1,17 @@
+# Megan Gagliardi & Rob Savoie
+# SYSA 3204
+# 11/5/2022
+# Lab 3 - Group 15
+
+from math import pi
+
+
+class Ellipse(object):
+    def ellipse_area(self, a, b):
+        area = pi * a * b
+
+        return area
+
+
+if __name__ == '__main__':
+    ellipse = Ellipse()
